7df37c75e50a61fe1834be6fccaedd04d47e1fd0
spark-roman <roman.akhunov@gmail.com>
2026-06-29 20:04:27 +0300

    fix namespaces

--- Mint.Database/Entities/Users/Sessions/UserSessionEntity.cs
+++ Mint.Database/Entities/Users/Sessions/UserSessionEntity.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Mint.Database.Entities.Bot.Commands;
 
 namespace Mint.Database.Entities.Users.Sessions;
 
