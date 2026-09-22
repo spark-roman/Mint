f1140507b1ccd44607c9c75b10ecca9523e1323c
spark-roman <roman.akhunov@gmail.com>
2026-09-23 00:16:09 +0300

    fix migration

---
+++ Mint.Database/Migrations/20260922211509_AddDraws.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mint.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDraws : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "total_draws",
+                table: "user_stats",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "ai_prompts",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3094), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2818), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2819), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 2,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2821), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2821), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 3,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2823), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2823), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 4,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2824), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2825), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 5,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2826), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2826), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 6,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2828), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(2828), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "steps",
+                keyColumn: "id",
+                keyValue: 2L,
+                column: "message",
+                value: "👤 **Ваш игровой профиль**\n━━━━━━━━━━━━━━━━━━━━━━━\n🏆 Ранг: {{rank_emoji}} **{{rank_name}}**\n👑 Очки по дуэлям: {{rank_points}}\n💰 Текущие очки: {{balance}} 🪙\n\n📊 **Статистика прогнозов**\n├ Всего: {{total_duels}}\n├ Успешно: {{wins}}\n├ Неудачно: {{losses}}\n├ Ничья: {{draws}}\n└ Точность: {{winrate}}%\n\n👥 **Рефералы**\n├ Приглашено: {{referral_count}}\n└ Всего получено: {{total_referral_bonus}} 🪙\n\n🎁 **Ежедневный бонус**\n├ Статус: {{bonus_status}}\n├ Дней подряд: {{streak_days}} 🔥\n└ Всего получено: {{total_daily_bonus}} 🪙\n\n📅 В игре с: {{member_since}}");
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 1L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3468), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 2L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3470), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 3L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3471), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 4L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3472), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 5L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3477), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 6L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3478), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 7L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3479), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 8L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3480), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 9L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3480), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 10L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 22, 21, 15, 8, 850, DateTimeKind.Unspecified).AddTicks(3481), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "total_draws",
+                table: "user_stats");
+
+            migrationBuilder.UpdateData(
+                table: "ai_prompts",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5894), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5688), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5689), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 2,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5690), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5691), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 3,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5692), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5693), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 4,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5694), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5694), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 5,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5696), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5696), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "bonus_types",
+                keyColumn: "id",
+                keyValue: 6,
+                columns: new[] { "created_at", "updated_at" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5697), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(5698), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "steps",
+                keyColumn: "id",
+                keyValue: 2L,
+                column: "message",
+                value: "👤 **Ваш игровой профиль**\n━━━━━━━━━━━━━━━━━━━━━━━\n🏆 Ранг: {{rank_emoji}} **{{rank_name}}**\n👑 Очки по дуэлям: {{rank_points}}\n💰 Текущие очки: {{balance}} 🪙\n\n📊 **Статистика прогнозов**\n├ Всего: {{total_duels}}\n├ Успешно: {{wins}}\n├ Неудачно: {{losses}}\n└ Точность: {{winrate}}%\n\n👥 **Рефералы**\n├ Приглашено: {{referral_count}}\n└ Всего получено: {{total_referral_bonus}} 🪙\n\n🎁 **Ежедневный бонус**\n├ Статус: {{bonus_status}}\n├ Дней подряд: {{streak_days}} 🔥\n└ Всего получено: {{total_daily_bonus}} 🪙\n\n📅 В игре с: {{member_since}}");
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 1L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6247), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 2L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6249), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 3L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6249), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 4L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6250), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 5L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6251), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 6L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6252), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 7L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6252), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 8L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6253), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 9L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6254), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "system_settings",
+                keyColumn: "id",
+                keyValue: 10L,
+                column: "updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 6, 11, 13, 7, 548, DateTimeKind.Unspecified).AddTicks(6255), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+    }
+}
